f99c88f48d5e169ca56a81d3e313d3c63e796a87
lilikwn <lilik1.wahyu7@gmail.com>
2021-04-01 09:50:52 +0700

    menambahkan beberapa class di Pertemuan5/Praktik

--- Pertemuan5/Praktik/MotorVehicle.java
+++ Pertemuan5/Praktik/MotorVehicle.java
@@ -2,11 +2,11 @@ class MotorVehicle extends Vehicle {
     public int sizeofEngine = 1;
     public String licencePlate = "H 1 IDN";
 
-    public motorVehicle(){
+    public MotorVehicle() {
 
     }
 
-    public MotorVehicle(int sizeEng, String lcPlate){
+    public MotorVehicle(int sizeEng, String lcPlate) {
         this.sizeofEngine = sizeEng;
         this.licencePlate = lcPlate;
     }
